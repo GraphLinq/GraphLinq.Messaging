b7fc6c3a5d8d0341c76ad3e88a5d549bbde9f3ac
jrbgit <joanas@gmail.com>
2021-04-05 17:54:23 -0400

    WIP: Adding Node For PushOver
    Adding a Node to interact with the PushOver push service.

---
+++ Nodes/PushOver/PushOverConnectorNode.cs
@@ -0,0 +1,51 @@
+﻿using NodeBlock.Engine;
+using NodeBlock.Engine.Attributes;
+using PushoverClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeBlock.Plugin.Messaging.Nodes.PushOver
+{
+    [NodeDefinition("PushOverConnectorNode", "PushOver Connector", NodeTypeEnum.Connector, "PushOver")]
+    [NodeGraphDescription("PushOver is a service that allow you to send push notification to your smartphone")]
+    public class PushOverConnectorNode : Node
+    {
+        public PushOverConnectorNode(string id, BlockGraph graph)
+           : base(id, graph, typeof(PushOverConnectorNode).Name)
+        {
+            this.CanBeSerialized = false;
+
+            this.InParameters.Add("apiKey", new NodeParameter(this, "apiKey", typeof(string), true));
+
+            this.OutParameters.Add("pushOver", new NodeParameter(this, "pushOver", typeof(PushOverConnectorNode), true));
+        }
+
+        public PushoverClient Client { get; set; }
+
+        public override bool CanBeExecuted => false;
+
+        public override bool CanExecute => true;
+
+        public override void SetupConnector()
+        {
+            Client = new Pushover(this.InParameters["apiKey"].GetValue().ToString());
+            this.Next();
+        }
+
+        public override void OnStop()
+        {
+
+        }
+
+        public override object ComputeParameterValue(NodeParameter parameter, object value)
+        {
+            if (parameter.Name == "pushOver")
+            {
+                return this;
+            }
+
+            return base.ComputeParameterValue(parameter, value);
+        }
+    }
+}
---
+++ Nodes/PushOver/SendPushAllDevicesNode.cs
@@ -0,0 +1,43 @@
+﻿using NodeBlock.Engine;
+using NodeBlock.Engine.Attributes;
+using PushoverClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeBlock.Plugin.Messaging.Nodes.PushOver
+{
+    [NodeDefinition("SendPushAllDevicesNode", "Send Push All Devices", NodeTypeEnum.Function, "PushOver")]
+    [NodeGraphDescription("Send a push via PushOver on all your devices")]
+    public class SendPushAllDevicesNode : Node
+    {
+        public SendPushAllDevicesNode(string id, BlockGraph graph)
+            : base(id, graph, typeof(SendPushAllDevicesNode).Name)
+        {
+            this.InParameters = new Dictionary<string, NodeParameter>()
+            {
+                { "pushOver", new NodeParameter(this, "pushOver", typeof(PushOverConnectorNode), true) },
+                { "title", new NodeParameter(this, "title", typeof(string), true) },
+                { "message", new NodeParameter(this, "message", typeof(string), true) }
+            };
+        }
+
+        public override bool CanExecute => true;
+        public override bool CanBeExecuted => true;
+
+        public override bool OnExecution()
+        {
+            PushOverConnectorNode pushOverConnector = this.InParameters["pushOver"].GetValue() as PushOverConnectorNode;
+            var title = this.InParameters["title"].GetValue().ToString();
+            var message = this.InParameters["message"].GetValue().ToString();
+
+            List<String> request = new List<String>() {
+                title, message
+            };
+
+            PushResponse response = pushOverConnector.Client.Push(request);
+
+            return true;
+        }
+    }
+}
