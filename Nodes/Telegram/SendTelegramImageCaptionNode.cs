b705583e7244f3c0fcd082e4f4c3635cde35a3c6
jr00t <joanas@gmail.com>
2022-03-22 11:05:07 -0400

    telegram photo and photo w/ caption blocks

---
+++ Nodes/Telegram/SendTelegramImageCaptionNode.cs
@@ -0,0 +1,41 @@
+﻿using NodeBlock.Engine;
+using NodeBlock.Engine.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeBlock.Plugin.Messaging.Nodes.Telegram
+{
+    [NodeDefinition("SendTelegramImageCaptionNode", "Send Telegram Photo With Caption", NodeTypeEnum.Function, "Telegram")]
+    [NodeGraphDescription("Send a photo on Telegram with a caption")]
+    public class SendTelegramImageCaptionNode : Node
+    {
+        public SendTelegramImageCaptionNode(string id, BlockGraph graph)
+            : base(id, graph, typeof(SendTelegramImageCaptionNode).Name)
+        {
+            this.InParameters = new Dictionary<string, NodeParameter>()
+            {
+                { "telegramBot", new NodeParameter(this, "telegramBot", typeof(TelegramBotInstanceNode), true) },
+                { "chatId", new NodeParameter(this, "chatId", typeof(string), true) },
+                { "photo", new NodeParameter(this, "photo", typeof(string), true) },
+                { "caption", new NodeParameter(this, "caption", typeof (string), true) }
+            };
+
+            this.OutParameters = new Dictionary<string, NodeParameter>()
+            {
+
+            };
+        }
+
+        public override bool CanExecute => true;
+        public override bool CanBeExecuted => true;
+
+        public override bool OnExecution()
+        {
+            TelegramBotInstanceNode telegramBotInstance = this.InParameters["telegramBot"].GetValue() as TelegramBotInstanceNode;
+            telegramBotInstance.Bot.SendPhotoAsync(long.Parse(this.InParameters["chatId"].GetValue().ToString()), this.InParameters["photo"].GetValue().ToString(), this.InParameters["caption"].GetValue().ToString());
+
+            return true;
+        }
+    }
+}
---
+++ Nodes/Telegram/SendTelegramImageNode.cs
@@ -0,0 +1,41 @@
+﻿using NodeBlock.Engine;
+using NodeBlock.Engine.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeBlock.Plugin.Messaging.Nodes.Telegram
+{
+    [NodeDefinition("SendTelegramImageNode", "Send Telegram Photo", NodeTypeEnum.Function, "Telegram")]
+    [NodeGraphDescription("Send a photo on Telegram")]
+    public class SendTelegramImageNode : Node
+    {
+        public SendTelegramImageNode(string id, BlockGraph graph)
+            : base(id, graph, typeof(SendTelegramImageNode).Name)
+        {
+            this.InParameters = new Dictionary<string, NodeParameter>()
+            {
+                { "telegramBot", new NodeParameter(this, "telegramBot", typeof(TelegramBotInstanceNode), true) },
+                { "chatId", new NodeParameter(this, "chatId", typeof(string), true) },
+                { "photo", new NodeParameter(this, "photo", typeof(string), true) }
+            };
+
+            this.OutParameters = new Dictionary<string, NodeParameter>()
+            {
+
+            };
+        }
+
+        public override bool CanExecute => true;
+        public override bool CanBeExecuted => true;
+
+        public override bool OnExecution()
+        {
+            TelegramBotInstanceNode telegramBotInstance = this.InParameters["telegramBot"].GetValue() as TelegramBotInstanceNode;
+            //telegramBotInstance.Bot.SendTextMessageAsync(long.Parse(this.InParameters["chatId"].GetValue().ToString()), this.InParameters["message"].GetValue().ToString(),
+             //   global::Telegram.Bot.Types.Enums.ParseMode.Html);
+            telegramBotInstance.Bot.SendPhotoAsync(long.Parse(this.InParameters["chatId"].GetValue().ToString()), this.InParameters["photo"].GetValue().ToString());
+            return true;
+        }
+    }
+}
